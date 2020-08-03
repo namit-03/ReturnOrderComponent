86694938d19dde55a9ffd05c3805c36cb252fab1
Groupcreation <68987062+Groupcreation@users.noreply.github.com>
2020-08-03 23:16:47 +0530

    Parameter return type changed from object to bool

--- Processing/IProcessing.cs
+++ Processing/IProcessing.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,7 +8,7 @@ namespace ComponentProcessingMicroservice.Processing
 {
     public interface IProcessing
     {
-         int ProcessingCharge(ProcessRequest ob1);
+         int ProcessingCharge(bool ob1);
 
     }
 }
